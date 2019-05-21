57a98c85847502d972593c28fd586c5a15451190
henokali1 <henokali1@gmail.com>
2019-05-21 17:24:36 +0400

    Progress Bar django starter

--- bin_django/bin/models.py
+++ bin_django/bin/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Student(models.Model):
     full_name = models.CharField(max_length=250, default='')
     score = models.IntegerField(default=0)
+    last_ts = models.IntegerField(default=0)
+    current_ts = models.IntegerField(default=0)
     gender = models.CharField(max_length=10, default='')
     prof_img = models.CharField(max_length=250, default='')
     id_num = models.CharField(max_length=250, default='')
