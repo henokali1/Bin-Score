bbae173fdeb3afc27c8cc3bbc2274b7caffd1c53
henokali1 <henokali1@gmail.com>
2019-05-24 16:05:19 +0400

    Old files removed

--- launcher.sh
+++ launcher.sh
@@ -3,9 +3,9 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/Desktop/Bin-Score
+cd /home/pi/Bin-Score
 
-python3 serial_data.py & python3 app.py & chromium-browser --app=http://127.0.0.1:5000/ --start-fullscreen
+# python3 serial_data.py & python3 app.py & chromium-browser --app=http://127.0.0.1:5000/ --start-fullscreen
 #chromium-browser --app=http://127.0.0.1:5000/ --start-fullscreen
-#python3 app.py
+python3 play_ground.py
 #cd /
